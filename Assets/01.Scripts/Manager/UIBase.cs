7c16ee980788fff9f8c850b9cf547dce9818a319
Jeon Sehun <twilivez@gmail.com>
2020-10-18 05:26:01 +0900

    [UPDATE] 모듈 불러오면서 중첩 돼 버린 파일 삭제

--- Assets/01.Scripts/Manager/UIBase.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIBase : SingletonObject<UIBase>
-{
-    public virtual void OpenUI(params object[] args) { }
-    public virtual void CloseUI(params object[] args) { }
-}
