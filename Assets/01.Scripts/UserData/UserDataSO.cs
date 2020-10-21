f676f786c63727e2490b7c83e3202c54f2062967
Jeon Sehun <twilivez@gmail.com>
2020-10-22 00:40:58 +0900

    [ADD] Singleton ScriptableObject로 UserData 구성

---
+++ Assets/01.Scripts/UserData/UserDataSO.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class UserData : ScriptableObject {
+    private static UserData instance;
+    public static UserData Instance {
+        get {
+            if (instance is null) { 
+                var userData = Resources.Load<UserData>("GameData/UserData");
+                if (userData is null) {
+                    var path = "Assets/Resources/GameData";
+
+                    var prefabsFolder = new DirectoryInfo(path);
+                    if (prefabsFolder.Exists == false) {
+                        prefabsFolder.Create();
+                    }
+
+                    userData = ScriptableObject.CreateInstance<UserData>();
+                    AssetDatabase.CreateAsset(userData, $"{path}/UserData.asset");
+                }
+
+                instance = userData;
+            }
+
+            return instance;
+        }
+    }
+
+    private uint coin;
+    public uint Coin {
+        get => coin;
+        set => coin = value;
+    }
+}
