ea4842d7b092083809addcc5555ec5665b4470e9
Park Changyong <tempus157@gmail.com>
2020-10-22 17:06:58 +0900

    [UPDATE] 자동으로 올라가는 코인

--- Assets/01.Scripts/Game/Clicker.cs
+++ Assets/01.Scripts/Game/Clicker.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Clicker : MonoBehaviour {
@@ -12,9 +14,18 @@ public class Clicker : MonoBehaviour {
     private Text coinText;
 
     private int coin;
+    private Tweener coinTweener;
 
     public void IncreaseCoin() {
         coin += increment;
         coinText.text = coin.ToString();
+        coinTweener.ChangeValues(coin, coin + autoIncrement);
+        coinTweener.Restart();
+    }
+
+    private void Awake() {
+        coinTweener = DOTween.To(() => coin, value => coin = value, coin + autoIncrement, 1f)
+                             .OnUpdate(() => coinText.text = coin.ToString()).SetEase(Ease.Linear)
+                             .SetLoops(-1, LoopType.Incremental);
     }
 }
