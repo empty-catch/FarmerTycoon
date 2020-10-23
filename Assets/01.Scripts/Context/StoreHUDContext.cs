a5720d6a12202ad4c082d12e68909ecaad2cd4df
Jeon Sehun <twilivez@gmail.com>
2020-10-24 03:30:56 +0900

    [UPDATE] 선택한 탭에 따라 Store 창 띄어주게 변경

--- Assets/01.Scripts/Context/StoreHUDContext.cs
+++ Assets/01.Scripts/Context/StoreHUDContext.cs
@@ -1,17 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Slash.Unity.DataBind.Core.Data;
 using UnityEngine;
 
 public class StoreHUDContext : Context {
-    private ItemType currentTap;
+    private Property<bool>isCurrentTapClosetProperty = new Property<bool>();
+    public bool IsCurrentTapCloset {
+        get => isCurrentTapClosetProperty.Value;
+        set => isCurrentTapClosetProperty.Value = value;
+    }    
+    
+    private Property<bool>isCurrentTapAnimalProperty = new Property<bool>();
+    public bool IsCurrentTapAnimal {
+        get => isCurrentTapAnimalProperty.Value;
+        set => isCurrentTapAnimalProperty.Value = value;
+    }    
+    
+    private Property<bool>isCurrentTapPlantProperty = new Property<bool>();
+    public bool IsCurrentTapPlant {
+        get => isCurrentTapPlantProperty.Value;
+        set => isCurrentTapPlantProperty.Value = value;
+    }    
+    
+    private Property<bool>isCurrentTapToolProperty = new Property<bool>();
+    public bool IsCurrentTapTool {
+        get => isCurrentTapToolProperty.Value;
+        set => isCurrentTapToolProperty.Value = value;
+    }
     
     public StoreHUDContext() {
-        currentTap = ItemType.Closet;
+        ChangeTap(0);
     }
 
-    public void ChangeTap(int index) {
-        currentTap = (ItemType)index;
+    public void ChangeTap(Single index) {
+        // TODO : 이거 리스트에 담고 해도 안되는데 좀 개극혐이라 추후 수정 
+        IsCurrentTapCloset = false;
+        IsCurrentTapAnimal = false;
+        IsCurrentTapPlant = false;
+        IsCurrentTapTool = false;
+
+        switch (index){
+            case 0:
+                IsCurrentTapCloset = true;
+                break;
+            case 1:
+                IsCurrentTapAnimal = true;
+                break;
+            case 2:
+                IsCurrentTapPlant = true;
+                break;
+            case 3:
+                IsCurrentTapTool = true;
+                break;
+            default:
+                throw new ArgumentException("Invalid format argument.");
+                break;
+        }
     }
     
     public void CloseStore() {
