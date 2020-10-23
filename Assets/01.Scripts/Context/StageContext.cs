e2578a5d0c18993aed50f2aac8995d4ff751245d
Park Changyong <tempus157@gmail.com>
2020-10-23 23:28:29 +0900

    [UPDATE] 스테이지 선택화면 텍스트

--- Assets/01.Scripts/Context/StageContext.cs
+++ Assets/01.Scripts/Context/StageContext.cs
@@ -2,7 +2,7 @@
 
 public class StageContext : Context {
     private readonly Property<Collection<StageButtonContext>> stageButtonContestsProperty =
-        new Property<Collection<StageButtonContext>>();
+        new Property<Collection<StageButtonContext>>(new Collection<StageButtonContext>());
 
     public Collection<StageButtonContext> StageButtonContexts {
         get => stageButtonContestsProperty.Value;
@@ -10,13 +10,11 @@ public class StageContext : Context {
     }
 
     public StageContext() {
-        StageButtonContexts = new Collection<StageButtonContext> {
-            new StageButtonContext("경상도", 0),
-            new StageButtonContext("강원도", 1_200_000_000),
-            new StageButtonContext("경기도", 2_400_000_000),
-            new StageButtonContext("제주도", 4_800_000_000),
-            new StageButtonContext("서울", 8_000_000_000)
-        };
+        StageButtonContexts.Add(new StageButtonContext("경상도", 0));
+        StageButtonContexts.Add(new StageButtonContext("강원도", 1_200_000_000));
+        StageButtonContexts.Add(new StageButtonContext("경기도", 2_400_000_000));
+        StageButtonContexts.Add(new StageButtonContext("제주도", 4_800_000_000));
+        StageButtonContexts.Add(new StageButtonContext("서울", 8_000_000_000));
     }
 
     public void Open() {
