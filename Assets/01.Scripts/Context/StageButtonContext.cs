0c87011983c5a41a6f20f26d68476fe827ffe4d3
Park Changyong <tempus157@gmail.com>
2020-10-23 23:06:38 +0900

    [UPDATE] 스테이지 화면 프리팹으로

---
+++ Assets/01.Scripts/Context/StageButtonContext.cs
@@ -0,0 +1,21 @@
+﻿using Slash.Unity.DataBind.Core.Data;
+
+public class StageButtonContext : Context {
+    private readonly Property<string> stageNameProperty = new Property<string>();
+    private readonly Property<ulong> costProperty = new Property<ulong>();
+
+    public string StageName {
+        get => stageNameProperty.Value;
+        private set => stageNameProperty.Value = value;
+    }
+
+    public ulong Cost {
+        get => costProperty.Value;
+        private set => costProperty.Value = value;
+    }
+
+    public StageButtonContext(string stageName, ulong cost) {
+        StageName = stageName;
+        Cost = cost;
+    }
+}
