9d6296a00fa6f3c3a4322f0178df26b4b8d5591a
Park Changyong <tempus157@gmail.com>
2020-10-29 03:56:35 +0900

    [UPDATE] 널 에러 뜨는거 해결

--- Assets/01.Scripts/System/FarmerHandler.cs
+++ Assets/01.Scripts/System/FarmerHandler.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class FarmerHandler : SingletonObject<FarmerHandler> {
@@ -8,9 +9,11 @@ public class FarmerHandler : SingletonObject<FarmerHandler> {
     public void UpdateItem() {
         var tool = UserData.Instance.SelectTool;
         var costume = UserData.Instance.SelectCloset;
-        var trimmedCostume =  costume.ItemSprite.name.Replace("Costume", string.Empty);
+        var trimmedCostume = costume.Key == string.Empty
+            ? "Farmer"
+            : costume.ItemSprite.name.Replace("Costume", string.Empty);
 
-        var path = tool is null
+        var path = tool.Key == string.Empty
             ? $"Costumes/{costume.ItemSprite.name}"
             : $"CharactersWithTools/{trimmedCostume}/{trimmedCostume}{tool.ItemSprite.name}";
 
